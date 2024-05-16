c369d7d6805f195711c2cd50c7a9f6d428e2a724
daramola oluwatoyin <victoria.mnpdm@gmail.com>
2024-05-16 13:16:31 +0100

    line

--- console.py
+++ console.py
@@ -5,10 +5,15 @@ import cmd
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
+    def emptyline(self):
+        pass
+
     def do_quit(self, line):
+        """A command to graciously exit the console\n"""
         return True
     
     def do_EOF(self, line):
+        """Another clean way to exit the interpreter\n"""
         return True
 
 if __name__ == '__main__':
