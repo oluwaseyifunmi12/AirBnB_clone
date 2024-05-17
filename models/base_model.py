34ed637185ae18aebc179f9c8b23e6c35ea7989e
PonmileDaniel <pomiledaniel@gmail.com>
2024-05-17 11:36:26 +0100

    dictionary task 4

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,18 @@ from datetime import datetime
 class BaseModel():
     """A Base class model"""
     
-    def __init__(self):
-        """Initializing BaseMOdle instance"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """Initializing BaseModle instance"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.fromisoformat(value)
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
         
     def __str__(self):
         """Return string representation of BaseMOdel instance"""
@@ -26,5 +33,5 @@ class BaseModel():
         result = self.__dict__.copy()
         result['__class__'] = self.__class__.__name__
         result['created_at'] = self.created_at.isoformat()
-        result['update_at'] = self.updated_at.isoformat()
+        result['updated_at'] = self.updated_at.isoformat()
         return result
