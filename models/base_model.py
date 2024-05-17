2a11b0b4aaa7822875147322e92b8799b037d114
PonmileDaniel <pomiledaniel@gmail.com>
2024-05-17 10:12:18 +0100

    Airbnb project

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,23 @@
 #!/usr/bin/python3
 
+import uuid
+from datetime import datetime
+
 class BaseModel():
-    pass
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        result = self.__dict__.copy()
+        result['__class__'] = self.__class__.__name__
+        result['created_at'] = self.created_at.isoformat()
+        result['update_at'] = self.updated_at.isoformat()
+        return result
