b4e3c5ad62d9c6db44c6dc2ccdc806234d75da23
PonmileDaniel <pomiledaniel@gmail.com>
2024-05-21 15:45:45 +0100

    Readjusting the test file for test base model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,19 +1,42 @@
 #!/usr/bin/python3
 """ A unittest class for base_model"""
 
-
-import models
 import unittest
-from models.base_model import BaseModel
+from io import StringIO
+import sys
+import os
 from datetime import datetime
 
-class TestBaseModel_to_dict(unittest.TestCase):
-    """Unittest case for the to_dict method in BaseModel class"""
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+
+from models.base_model import BaseModel
+
+class TestBaseModel(unittest.TestCase):
+    """Unittest case for methods in BaseModel class"""
+
+    def test_save_method(self):
+        captured_output = StringIO()
+        sys.stdout = captured_output
+
+        model = BaseModel()
+        model.save()
+
+        sys.stdout = sys.__stdout__
+
+        self.assertEqual(captured_output.getvalue().strip(), "OK")
 
     def test_to_dict_instance(self):
         instance = BaseModel()
         result = instance.to_dict()
         self.assertIsInstance(result, dict)
+        self.assertEqual(result['__class__'], 'BaseModel')
+        self.assertEqual(result['created_at'], instance.created_at.isoformat())
+        self.assertEqual(result['updated_at'], instance.updated_at.isoformat())
+
+    def test_str_method(self):
+        model = BaseModel()
+        expected_str = f"[BaseModel] ({model.id}) {model.__dict__}"
+        self.assertEqual(str(model), expected_str)
 
 
 if __name__ == '__main__':
